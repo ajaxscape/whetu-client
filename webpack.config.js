e372adc6e18df07172232df7ce9b107a56358acf
Ben on windows <ben@ajaxscape.com>
2018-02-13 08:26:55 +0000

    Split whetu-render and whetu-client

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const { ifDevelopment, ifProduction } = getIfUtils(nodeEnv)
 
 module.exports = removeEmpty({
   entry: {
-    js: './src/index.js',
+    js: './index.js',
     css: './src/index.scss'
   },
 

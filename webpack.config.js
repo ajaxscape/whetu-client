6d05d01491245cf08e28a6803330f1a1deb8fdc1
Ben on windows <ben@ajaxscape.com>
2018-02-19 23:11:23 +0000

    Close but rendering stuck to the top

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
-const nodeEnv = process.env.NODE_ENV || 'production'
+const nodeEnv = process.env.NODE_ENV || 'development'
 const {ifDevelopment, ifProduction} = getIfUtils(nodeEnv)
 
 module.exports = removeEmpty({
